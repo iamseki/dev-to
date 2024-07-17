6fd3ae1703eb4489b8e730b146763f9904ce3412
iamseki <chrisyuri_19@hotmail.com>
2024-07-17 14:29:43 -0300

    feat: include benchmark for both naive and optimized parsing

---
+++ apps/processing-large-json-golang/cmd/parse-naive/benchmark_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"testing"
+
+	"github.com/iamseki/dev-to/apps/processing-large-json-golang/internal/parser"
+)
+
+func BenchmarkParseJSONNaive(b *testing.B) {
+	filename := "../../largefile-20240717-104907-20mb.json"
+
+	log.Println("Starting BenchmarkParseJSONNaive")
+	for i := 0; i < b.N; i++ {
+		parser.NaiveParseFile(filename)
+	}
+	log.Println("Finished BenchmarkParseJSONNaive")
+}
---
+++ apps/processing-large-json-golang/cmd/parse-optimized/benchmark_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"testing"
+
+	"github.com/iamseki/dev-to/apps/processing-large-json-golang/internal/parser"
+)
+
+func BenchmarkParseJSONOptimized(b *testing.B) {
+	filename := "../../largefile-20240717-104907-20mb.json"
+
+	log.Println("Starting BenchmarkParseJSONOptimized")
+	for i := 0; i < b.N; i++ {
+		parser.OptimizedParseFile(filename)
+	}
+	log.Println("Finished BenchmarkParseJSONOptimized")
+}
